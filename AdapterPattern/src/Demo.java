5ef29105dc142b61e5131b7335cd661fb6f42d58
Md. Akash Ahmed <86203010+AkashAhmed41@users.noreply.github.com>
2022-06-16 01:21:30 +0600

    AdapterPatternDemo added

---
+++ AdapterPattern/src/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        AudioPlayer audioPlayer = new AudioPlayer();
+
+        audioPlayer.play("mp3", "Love u like do.");
+        audioPlayer.play("mp4", "Why not me?");
+        audioPlayer.play("vlc", "10000 hours.");
+        audioPlayer.play("avi", "PcyPath.");
+    }
+}
